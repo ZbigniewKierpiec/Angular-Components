9c43a8bc48206550a9c072b8d23cb73d2e64db84
zee-creato <zbigniew.kierpiec@proav.com>
2024-03-11 10:14:55 +0000

    xxx

--- src/app/Components/Acordion/acordion/acordion.component.ts
+++ src/app/Components/Acordion/acordion/acordion.component.ts
@@ -41,37 +41,47 @@ import {
       transition('out => in', animate('400ms ease-in-out')),
     ]),
   ],
+
+
+
+
 })
 export class AcordionComponent implements OnInit {
   sections = [
     {
+      id:0,
       title: 'Section 1',
       content: 'Content for Section 1',
-      icon: 'fa-solid fa-chevron-down',
+      // icon: 'fa-solid fa-chevron-down',
       expanded: false,
     },
     {
+      id:1,
       title: 'Section 2',
       content: 'Content for Section 2',
-      icon: 'fa-solid fa-chevron-down',
+      // icon: 'fa-solid fa-chevron-down',
       expanded: false,
     },
     {
+      id:2,
       title: 'Section 3',
       content: 'Content for Section 3',
-      icon: 'fa-solid fa-chevron-down',
+      // icon: 'fa-solid fa-chevron-down',
       expanded: false,
     },
   ];
 
   expandedSectionIndex: number | null = null;
   active: boolean = true;
+  activeSection:number=0;
 
   toggleSection(index: number): void {
     if (this.expandedSectionIndex === index) {
       // Clicked on the already expanded section, so collapse it
       this.expandedSectionIndex = null;
       this.sections.forEach((f) => (f.expanded = true));
+
+
     } else {
       // Collapse the previously expanded section
       this.expandedSectionIndex = index;
@@ -83,5 +93,10 @@ export class AcordionComponent implements OnInit {
     return this.expandedSectionIndex === index;
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+
+
+
+  }
 }
