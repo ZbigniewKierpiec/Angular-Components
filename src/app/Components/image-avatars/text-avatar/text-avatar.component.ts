819f72b5e91c1674c112c5146e05e24a5fca3830
zee-creato <zbigniew.kierpiec@proav.com>
2024-05-02 10:16:17 +0100

    xxx

--- src/app/Components/image-avatars/text-avatar/text-avatar.component.ts
+++ src/app/Components/image-avatars/text-avatar/text-avatar.component.ts
@@ -3,16 +3,23 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-text-avatar',
   templateUrl: './text-avatar.component.html',
-  styleUrls: ['./text-avatar.component.scss']
+  styleUrls: ['./text-avatar.component.scss'],
 })
 export class TextAvatarComponent implements OnInit {
   // Define an input property
-@Input() title:string='';
-@Input() backgroundColor: string = '';
-@Input() color: string = '';
-  constructor() { }
+  @Input() title: string = '';
+  @Input() backgroundColor: string = '';
+  @Input() color: string = '';
+  @Input() size!: string;
+  fontSize: any = '';
 
-  ngOnInit(): void {
+  fontCalc() {
+    this.fontSize = parseInt(this.size) / 2;
   }
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.fontCalc();
+  }
 }
