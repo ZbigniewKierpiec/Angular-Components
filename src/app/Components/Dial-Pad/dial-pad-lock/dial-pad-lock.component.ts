9b937ba9790971fbc56065f554257f8e08436b81
zee-creato <zbigniew.kierpiec@proav.com>
2024-05-23 17:20:34 +0100

    xxx

--- src/app/Components/Dial-Pad/dial-pad-lock/dial-pad-lock.component.ts
+++ src/app/Components/Dial-Pad/dial-pad-lock/dial-pad-lock.component.ts
@@ -3,66 +3,54 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dial-pad-lock',
   templateUrl: './dial-pad-lock.component.html',
-  styleUrls: ['./dial-pad-lock.component.scss']
+  styleUrls: ['./dial-pad-lock.component.scss'],
 })
 export class DialPadLockComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-
-
- active:boolean=false;
-
+  active: boolean = false;
+  spansArray: number[] = [];
   count = 0;
-  outputDigits:string[]=[];
+  outputDigits: string[] = [];
   password: string = '';
   maxCount: number = 4;
+  eyeIcon: string = 'fa-eye-slash';
 
-
-
-see(){
-  this.active =! this.active;
-}
-
-
-
-
+  showPass() {
+    this.active = !this.active;
+    this.active ? (this.eyeIcon = 'fa-eye') : (this.eyeIcon = 'fa-eye-slash');
+  }
 
   addDigit(digit: string) {
-    console.log(digit)
+    console.log(digit);
     if (this.count < this.maxCount) {
- this.outputDigits.push(digit)
+      this.outputDigits.push(digit);
       this.updatePassword();
       this.count++;
     }
-
-
   }
 
   removeLastDigit() {
     if (this.password.length > 0) {
-        this.outputDigits.pop();
-         this.updatePassword()
-         this.count--;
+      this.outputDigits.pop();
+      this.updatePassword();
 
+      const numberSpans = document.querySelectorAll('.number');
+
+      this.count--;
     }
   }
-
 
   updatePassword(): void {
     this.password = this.outputDigits.join('');
   }
 
-clear(){
-   this.outputDigits=[];
-   this.count = 0;
-}
+  clear() {
+    this.outputDigits = [];
+    this.count = 0;
+  }
 
-
-
-
-
+  ngOnInit(): void {
+    this.spansArray = Array(this.maxCount).fill(0);
+  }
 }
