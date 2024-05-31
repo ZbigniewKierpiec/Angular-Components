e0b215df48cf74fec79539585550d173f0ddaa1e
zee-creato <zbigniew.kierpiec@proav.com>
2024-05-31 16:18:33 +0100

    xxx

--- src/app/Components/clock/clock.component.ts
+++ src/app/Components/clock/clock.component.ts
@@ -3,16 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-clock',
   templateUrl: './clock.component.html',
-  styleUrls: ['./clock.component.scss']
+  styleUrls: ['./clock.component.scss'],
 })
 export class ClockComponent implements OnInit {
-
-
   time: string = '';
   date: string = '';
   private week: string[] = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.updateTime();
@@ -21,8 +19,20 @@ export class ClockComponent implements OnInit {
 
   private updateTime(): void {
     const cd = new Date();
-    this.time = this.zeroPadding(cd.getHours(), 2) + ':' + this.zeroPadding(cd.getMinutes(), 2) + ':' + this.zeroPadding(cd.getSeconds(), 2);
-    this.date = this.zeroPadding(cd.getFullYear(), 4) + '-' + this.zeroPadding(cd.getMonth() + 1, 2) + '-' + this.zeroPadding(cd.getDate(), 2) + ' ' + this.week[cd.getDay()];
+    this.time =
+      this.zeroPadding(cd.getHours(), 2) +
+      ':' +
+      this.zeroPadding(cd.getMinutes(), 2) +
+      ':' +
+      this.zeroPadding(cd.getSeconds(), 2);
+    this.date =
+      this.zeroPadding(cd.getFullYear(), 4) +
+      '-' +
+      this.zeroPadding(cd.getMonth() + 1, 2) +
+      '-' +
+      this.zeroPadding(cd.getDate(), 2) +
+      ' ' +
+      this.week[cd.getDay()];
   }
 
   private zeroPadding(num: number, digit: number): string {
@@ -32,6 +42,4 @@ export class ClockComponent implements OnInit {
     }
     return (zero + num).slice(-digit);
   }
-
-
 }
